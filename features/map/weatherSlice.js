3f5afb81d3cc84d83dac1ce9e6eec5f4a61d9cf0
mertoztat <mertoztat@gmail.com>
2023-10-31 13:50:34 +0300

    clear localstorage

--- features/map/weatherSlice.js
+++ features/map/weatherSlice.js
@@ -6,7 +6,6 @@ export const weatherSlice = createSlice({
   name: "weather",
   initialState: {
     city: "izmir",
-    data: JSON.parse(localStorage.getItem("data") || null),
     weather: "",
   },
   reducers: {
@@ -14,19 +13,12 @@ export const weatherSlice = createSlice({
       state.city = action.payload;
     },
 
-    setData: (state, action) => {
-      if (action.payload) {
-        localStorage.setItem("data", JSON.stringify(action.payload));
-      } else {
-        localStorage.removeItem("data");
-      }
-    },
     setWeather: (state, action) => {
       state.weather = action.payload;
     },
   },
 });
 
-export const { setCity, setData, setWeather } = weatherSlice.actions;
+export const { setCity, setWeather } = weatherSlice.actions;
 
 export default weatherSlice.reducer;
